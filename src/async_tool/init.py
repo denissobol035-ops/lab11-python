473ede3cfc25acf3981cf6a2cc7d1ebbf4db9aef
denissobol035-ops <denissobol035@gmail.com>
2026-05-03 09:42:57 +0300

    Create init.py

---
+++ src/async_tool/init.py
@@ -0,0 +1,10 @@
+from .parser import parse_numbers
+from .analysis import analyze_numbers
+from .formatter import build_report, build_sorted_report
+
+__all__ = [
+    "parse_numbers",
+    "analyze_numbers",
+    "build_report",
+    "build_sorted_report",
+]
